6c4489f843fa57786a122e6d900d91cdbea0b79e
wangyuanchi <yuanchiri@gmail.com>
2025-08-21 17:40:14 +0800

    Add initial skeletal version

---
+++ src/main/java/Shibe.java
@@ -0,0 +1,7 @@
+public class Shibe {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Shibe.\n" +
+                "What can I do for you?\n" +
+                "Bye. Hope to see you again soon!");
+    }
+}

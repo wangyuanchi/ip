d21408d47e856ca83f8cf4c9cc2831fffc4423dd
wangyuanchi <yuanchiri@gmail.com>
2025-08-21 18:07:51 +0800

    Add echo

--- src/main/java/Shibe.java
+++ src/main/java/Shibe.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
+
 public class Shibe {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Shibe.\n" +
-                "What can I do for you?\n" +
-                "Bye. Hope to see you again soon!");
+                "What can I do for you?");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+
+            System.out.println(input);
+        }
+
+        scanner.close();
     }
 }

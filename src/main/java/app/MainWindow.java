50bb8af08c14b1ff27442bbe3e76de0a38c431ad
wangyuanchi <yuanchiri@gmail.com>
2025-09-04 19:28:57 +0800

    Transfer code from JavaFX template

---
+++ src/main/java/app/MainWindow.java
@@ -0,0 +1,52 @@
+package app;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+/**
+ * Controller for the main GUI.
+ */
+public class MainWindow extends AnchorPane {
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Button sendButton;
+
+    private Shibe shibe;
+
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/user.png"));
+    private Image shibeImage = new Image(this.getClass().getResourceAsStream("/images/shibe.png"));
+
+    @FXML
+    public void initialize() {
+        scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+    }
+
+    /** Injects the Shibe instance */
+    public void setShibe(Shibe s) {
+        shibe = s;
+    }
+
+    /**
+     * Creates two dialog boxes, one echoing user input and the other containing
+     * Shibe's reply and then appends them to the dialog container. Clears the user
+     * input after processing.
+     */
+    @FXML
+    private void handleUserInput() {
+        String input = userInput.getText();
+        String response = shibe.getResponse(input);
+        dialogContainer.getChildren().addAll(DialogBox.getUserDialog(input, userImage),
+                DialogBox.getShibeDialog(response, shibeImage));
+        userInput.clear();
+    }
+}
